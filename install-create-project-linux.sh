028dc94eadfa53c9dd4b61b4e82df7e07461c28a
stringmanolo <manuelvarelacaldas@gmail.com>
2021-10-18 19:53:42 +0200

    Install output

--- install-create-project-linux.sh
+++ install-create-project-linux.sh
@@ -1,3 +1,5 @@
+echo "Appending Termux/Linux compatibility fix to $HOME/.bashrc";
+
 echo '
 # <create-project-fix>
 
@@ -19,11 +21,28 @@ createProjectAddPath "$PREFIX/bin/";
 # </create-project-fix>
 ' >> "$HOME/.bashrc"
 
+echo "Setting up PREFIX path...";
+PREFIX="$HOME/data/data/com.termux/files/usr"
 
-PREFIX="$HOME/data/data/com.termux/files/usr" && yes | rm "./create-project" "$PREFIX/include/create-project-templates" "$PREFIX/bin/create-project" -r 2>&1 > /dev/null;
-git clone https://github.com/StringManolo/create-project;
-chmod +775 create-project/export/create-project/;
+echo "Removing older create-project folder...";
+yes | rm "./create-project" "$PREFIX/include/create-project-templates" "$PREFIX/bin/create-project" -r 2>&1 > /dev/null;
+
+echo "Downloading create-project files...";
+git clone https://github.com/StringManolo/create-project
+
+echo "Giving execution permissions to create-project command...";
+chmod +775 create-project/export/create-project;
+
+echo "Creating bin folder to run command without root...";
 mkdir -p "$PREFIX/bin";
+
+echo "Moving command to bin folder..."
 mv create-project/export/create-project "$PREFIX/bin/";
+
+echo "Moving default templates to include folder...";
 mv create-project/export/create-project-templates "$PREFIX/include/";
-# yes | rm create-project -r;
+
+echo "Removing installation files...";
+yes | rm create-project -r;
+
+echo "Fix ready to apply. Source the .bashrc file...";
