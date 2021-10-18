3b89671205e337d4ab5dcbbdc92b564311f8b7f4
stringmanolo <manuelvarelacaldas@gmail.com>
2021-10-18 19:30:47 +0200

    Upload Linux installation file

--- install-create-project-linux.sh
+++ install-create-project-linux.sh
@@ -1,15 +1,23 @@
 echo '
+# <create-project-fix>
+
 # THIS IS A FIX TO INSTALL https://github.com/stringmanolo/create-project on Linux distributions. Design was originally made for Termux.
 
-addPath() {
+createProjectAddPath() {
   if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
     PATH="${PATH:+"$PATH:"}$1";
   fi
 }
 
-PREFIX="$HOME/data/data/com.termux/files/usr" && yes | rm "./create-project" "$PREFIX/include/create-project-templates" "$PREFIX/bin/create-project" -r 2>&1 > /dev/null ; git clone https://github.com/StringManolo/create-project && chmod +775 create-project/export/create-project && mkdir -p "$PREFIX/bin" && mv create-project/export/create-project "$PREFIX/bin/" && mv create-project/export/create-project-templates "$PREFIX/include/" && yes | rm create-project -r;
+PREFIX="$HOME/data/data/com.termux/files/usr";
 
 export PREFIX="$PREFIX";
 
-addPath "$PREFIX/bin/";
+createProjectAddPath "$PREFIX/bin/";
+
+
+# </create-project-fix>
 ' >> "$HOME/.bashrc"
+
+
+PREFIX="$HOME/data/data/com.termux/files/usr" && yes | rm "./create-project" "$PREFIX/include/create-project-templates" "$PREFIX/bin/create-project" -r 2>&1 > /dev/null; git clone https://github.com/StringManolo/create-project && chmod +775 create-project/export/create-project && mkdir -p "$PREFIX/bin" && mv create-project/export/create-project "$PREFIX/bin/" && mv create-project/export/create-project-templates "$PREFIX/include/" && yes | rm create-project -r;
